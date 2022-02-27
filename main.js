647d35db094e1c6e96e6e5d57f23dfa8aec3b558
Hassanat Busari <60980025+HassanatB-O@users.noreply.github.com>
2022-02-27 12:16:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 input = document.getElementById("input");
 var makeConversion = function() {    
     userInput = input.value;
-    document.getElementById("meters").textContent = userInput + " meters = " + (userInput * 3.28084).toFixed(3) + " feet";
+    document.getElementById("meters").textContent = userInput + " meter(s) = " + (userInput * 3.28084).toFixed(3) + " feet";
 
-    document.getElementById("feet").textContent = userInput + " feet = " + (userInput * 0.3048).toFixed(3) + " meters";
+    document.getElementById("feet").textContent = userInput + " feet = " + (userInput * 0.3048).toFixed(3) + " meter(s)";
     
-    document.getElementById("lts").textContent = userInput + " liters = " + (userInput * 0.264172).toFixed(3) + " gallons";
+    document.getElementById("lts").textContent = userInput + " liter(s) = " + (userInput * 0.264172).toFixed(3) + " gallon(s)";
     
-    document.getElementById("gallons").textContent = userInput + " gallons = " + (userInput * 3.78541).toFixed(3) + " liters";
+    document.getElementById("gallons").textContent = userInput + " gallon(s) = " + (userInput * 3.78541).toFixed(3) + " liter(s)";
     
     document.getElementById("kg").textContent = userInput + " kilos = " + (userInput * 2.20462).toFixed(3) + " pounds";
     
-    document.getElementById("pounds").textContent = userInput + " pounds = " + (userInput * 0.453592).toFixed(3) + " kilos";
+    document.getElementById("pounds").textContent = userInput + " pound(s) = " + (userInput * 0.453592).toFixed(3) + " kilo(s)";
     
     document.getElementById("ms").textContent = userInput + " m/s = " + (userInput * 3.6).toFixed(3) + " km/h";
     
